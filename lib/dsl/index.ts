49362f7b3b35d9a77eab6e9a3f668260763c29c8
PENEKhun <penekhun@gmail.com>
2025-03-11 22:32:56 +0900

    style: 불필요한 주석 제거
    심지어 @example보면 용례가 잘못 표기 되어 있습니다.

--- lib/dsl/index.ts
+++ lib/dsl/index.ts
@@ -14,23 +14,6 @@
  * limitations under the License.
  */
 
-/**
- * itdoc - Test-driven API Documentation Generator
- * @description
- * API 테스트 코드를 기반으로 문서를 자동 생성하는 라이브러리입니다.
- * @example
- * ```typescript
- * import { describeAPI, itDoc, HttpMethod } from 'itdoc';
- *
- * describeAPI('User API', () => {
- *   itDoc('should create a user')
- *     .method(HttpMethod.POST)
- *     .path("/users")
- *     .expect(201);
- * });
- * ```
- */
-
 export { HttpMethod } from "./enums/HttpMethod"
 export { HttpStatus } from "./enums/HttpStatus"
 export { describeAPI, itDoc, field } from "./interface"
