b4118dfdd8f5c99c416a7b0df3c04b6ab707ea8a
PENEKhun <penekhun@gmail.com>
2025-03-23 17:39:34 +0900

    style: 불필요한 주석 제거

--- lib/dsl/adapters/TestFramework.ts
+++ lib/dsl/adapters/TestFramework.ts
@@ -22,17 +22,3 @@ export enum TestFramework {
     Mocha = "MOCHA",
     Unknown = "UNKNOWN",
 }
-
-// /**
-//  * 지원하는 테스트 프레임워크 타입
-//  */
-// export type TestFramework = "Unknown" | "Jest" | "Mocha";
-
-// /**
-//  * 테스트 프레임워크 상수
-//  */
-// export const TestFramework = {
-//     Unknown: "Unknown" as const,
-//     Jest: "Jest" as const,
-//     Mocha: "Mocha" as const,
-// } as const;
