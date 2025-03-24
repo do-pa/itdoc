929adcd521c1902b14382d1e86c27154588383a0
wnghdcjfe <jhc9639@naver.com>
2025-03-25 08:47:10 +0900

    Feat/doc generation : update by review

--- script/makedocs/index.ts
+++ script/makedocs/index.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { execSync } from "child_process"
 import { existsSync, mkdirSync } from "fs"
 import { fileURLToPath } from "url"
@@ -34,8 +50,6 @@ const printHeader = (title: string): void => {
  * @constant {string}
  */
 const outputDir: string = join(__dirname, "../", "output")
-
-// 출력 디렉토리가 존재하지 않으면 생성합니다.
 if (!existsSync(outputDir)) {
     mkdirSync(outputDir, { recursive: true })
     console.log(chalk.green(`디렉토리 생성됨: ${outputDir}`))
@@ -47,22 +61,16 @@ if (!existsSync(outputDir)) {
  * @constant {string}
  */
 const openapiPath: string = join(__dirname, "../oas/openapi.yaml")
-
-// OpenAPI 파일이 존재하지 않으면 에러 메시지를 출력하고 종료합니다.
 if (!existsSync(openapiPath)) {
     console.error(chalk.red(`오류: OAS 파일을 찾을 수 없습니다. 경로: ${openapiPath}`))
     process.exit(1)
 }
 
 try {
-    // 스크립트 시작을 알리는 헤더 출력
     printHeader("ITDOC 스크립트 시작")
 
     console.log(chalk.yellow("Step 1: OpenAPI YAML 파일을 Markdown으로 변환 중"))
     const markdownPath = join(outputDir, "output.md")
-    /**
-     * npx widdershins 명령어를 실행하여 OpenAPI YAML 파일을 Markdown으로 변환합니다.
-     */
     execSync(`npx widdershins "${openapiPath}" -o "${markdownPath}"`, {
         stdio: "inherit",
         cwd: __dirname,
@@ -71,19 +79,13 @@ try {
 
     console.log(chalk.yellow("Step 2: Redocly CLI를 사용하여 HTML 문서 생성 중"))
     const htmlPath = join(outputDir, "redoc.html")
-    /**
-     * npx @redocly/cli 명령어를 실행하여 OpenAPI YAML 파일을 HTML 문서로 변환합니다.
-     */
     execSync(`npx @redocly/cli build-docs "${openapiPath}" --output "${htmlPath}"`, {
         stdio: "inherit",
         cwd: __dirname,
     })
     console.log(chalk.green(`HTML 문서 생성 완료: ${htmlPath}\n`))
-
-    // 모든 작업 완료를 알리는 헤더 출력
     printHeader("모든 작업이 성공적으로 완료되었습니다.")
 } catch (error: unknown) {
-    // 오류 발생 시, 에러 메시지를 출력하고 프로세스를 종료합니다.
     console.error(chalk.red("오류 발생:"), error)
     process.exit(1)
 }
