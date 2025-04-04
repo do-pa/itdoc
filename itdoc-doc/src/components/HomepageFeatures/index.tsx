8c65d3fa45905b00693a7fdf393818d7670cd750
wnghdcjfe <jhc9639@naver.com>
2025-04-04 19:04:44 +0900

    docs: itdoc문서초안작업 relate #14

---
+++ itdoc-doc/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,60 @@
+import type { ReactNode } from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  image: string;
+  description: ReactNode;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Easy to Use',
+    image: require('@site/static/img/a.png').default,
+    description: <>itdoc은 API 테스트를 매우 쉽게 만들어 줍니다. 인터페이스 자체가 정말 쉬워요!</>,
+  },
+  {
+    title: 'Focus on What Matters',
+    image: require('@site/static/img/b.png').default,
+    description: (
+      <>
+        사실 API 테스트란 만들기도 귀찮고 문서화시키기도 어렵죠. 이를 itdoc을 통한다면 테스트를 만들 뿐만 아니라 문서도 쉽게 만들 수 있습니다.
+      </>
+    ),
+  },
+  {
+    title: 'For JS and TS',
+    image: require('@site/static/img/c.png').default,
+    description: <>itdoc은 자바스크립트와 타입스크립트 API 테스팅에 중점을 맞춘 테스트를 만들어주는 도구입니다.</>,
+  },
+];
+
+function Feature({ title, image, description }: FeatureItem): JSX.Element {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <img src={image} className={styles.featureSvg} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <div>{description}</div>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
