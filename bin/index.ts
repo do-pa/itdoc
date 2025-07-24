1334151b54e34d21929396345927d7aa5c76a1f9
zagabi <jhc9639@naver.com>
2025-07-24 10:50:01 +0900

    feat: improve CLI output (#216)
    related #177

--- bin/index.ts
+++ bin/index.ts
@@ -22,18 +22,43 @@ import { resolvePath } from "../lib/utils/pathResolver"
 
 const args = process.argv.slice(2)
 const isRootHelp = args.length === 0 || args[0] === "--help" || args[0] === "-h"
+const printBanner = () => {
+    console.log(
+        ` 
+ ___  _________  ________  ________  ________          ___       ___       _____ ______      
+|\\  \\|\\___   ___|\\   ___ \\|\\   __  \\|\\   ____\\        |\\  \\     |\\  \\     |\\   _ \\  _   \\    
+\\ \\  \\|___ \\  \\_\\ \\  \\_|\\ \\ \\  \\|\\  \\ \\  \\___|        \\ \\  \\    \\ \\  \\    \\ \\  \\\\\\__\\ \\  \\   
+ \\ \\  \\   \\ \\  \\ \\ \\  \\ \\\\ \\ \\  \\\\\\  \\ \\  \\            \\ \\  \\    \\ \\  \\    \\ \\  \\\\|__| \\  \\  
+  \\ \\  \\   \\ \\  \\ \\ \\  \\_\\\\ \\ \\  \\\\\\  \\ \\  \\____        \\ \\  \\____\\ \\  \\____\\ \\  \\    \\ \\  \\ 
+   \\ \\__\\   \\ \\__\\ \\ \\_______\\ \\_______\\ \\_______\\       \\ \\_______\\ \\_______\\ \\__\\    \\ \\__\\
+    \\|__|    \\|__|  \\|_______|\\|_______|\\|_______|        \\|_______|\\|_______|\\|__|     \\|__|
+                                                                                                                                                         
+    `,
+    )
+}
 if (isRootHelp) {
-    logger.box("ITDOC CLI")
+    printBanner()
 }
 const program = new Command()
+
+program
+    .name("itdoc")
+    .usage("A CLI tool for generating ITDOC test code using LLM") // ✅ 직접 Usage 설정
+    .addHelpText(
+        "after",
+        `
+Example:
+  itdoc generate -a <appPath>
+`,
+    )
+
 program
     .command("generate")
     .description("Generate ITDOC test code based on LLM.")
-    .option("-p, --path <testspecPath>", "Path to the markdown test spec file.", undefined)
-    .option("-a, --app <appPath>", "Path to the Express root app file.", undefined)
-    .option("-e, --env <envPath>", "Path to the .env file.", undefined)
+    .option("-p, --path <testspecPath>", "Path to the markdown test spec file.")
+    .option("-a, --app <appPath>", "Path to the Express root app file.")
+    .option("-e, --env <envPath>", "Path to the .env file.")
     .action((options: { path?: string; env?: string; app?: string }) => {
-        // Load .env
         const envPath = options.env
             ? path.isAbsolute(options.env)
                 ? options.env
@@ -42,12 +67,13 @@ program
 
         if (!options.path && !options.app) {
             logger.error(
-                "Either a test spec path (-p) or an Express app path (-a) must be provided.",
+                "Either a test spec path (-p) or an Express app path (-a) must be provided. By default, the OpenAI key (OPENAI_API_KEY in .env) is loaded from the root directory, but you can customize the path if needed",
             )
-            logger.info("Example: itdoc generate -p ../md/testspec.md")
-            logger.info("Example: itdoc generate --path ../md/testspec.md")
-            logger.info("Example: itdoc generate -a ../app.js")
-            logger.info("Example: itdoc generate --app ../app.js")
+            logger.info("ex) itdoc generate -p ../md/testspec.md")
+            logger.info("ex) itdoc generate --path ../md/testspec.md")
+            logger.info("ex) itdoc generate -a ../app.js")
+            logger.info("ex) itdoc generate --app ../app.js")
+            logger.info("ex) itdoc generate -a ../app.js -e <custom path : env>")
             process.exit(1)
         }
 
